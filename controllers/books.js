60afe80b2498b24fbca79df1d2f6c6fd93d3e8a3
gokepelemo <goke.pelemo@gmail.com>
2023-09-18 14:55:49 -0500

    use node fetch in node.js

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fetch = 'node-fetch-commonjs';
+const fetch = 'node-fetch';
 
 const Book = require("../models/book");
 const Vote = require("../models/vote");
